322e8853cad0c6898db7cf8838c04dd381e4bc9c
JoaoAlves20 <jvsimoesalves@gmail.com>
2026-05-01 19:10:15 -0300

    criei todo o crud no controller

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -12,6 +12,63 @@ class UserController {
 
         response.status(200).json(data);
     }
+
+    async findById(request: Request, response: Response) {
+        const { id } = request.params;
+        
+        const data = await UserService.findById(id as string);
+
+        if (!data) {
+            return response.status(404).json({ error: 'User not found' });
+        }
+
+        response.status(200).json(data);
+    }
+
+    async create(request: Request, response: Response) {
+        const user_create = request.body;
+
+        if (!user_create.username || !user_create.email || !user_create.password) {
+            return response.status(400).json({ error: 'username, password and email is requireds' });
+        }
+
+        const success = await UserService.create(user_create);
+
+        if (!success) {
+            return response.status(400).json({ error: 'error in created user' });
+        }
+
+        response.status(201).json({ success: 'created user' });
+    }
+
+    async update(request: Request, response: Response) {
+        const body_updated = request.body;
+        const { id } = request.params;
+
+        if (!body_updated) {
+            return response.status(400).json({ error: 'needed of body to update user' });
+        }
+
+        const success = await UserService.update(id as string, body_updated);
+
+        if (!success) {
+            return response.status(400).json({ error: 'error in updated user' });
+        }
+
+        response.status(200).json({ success: 'updated user' });
+    }
+
+    async delete(request: Request, response: Response) {
+        const { id } = request.params;
+
+        const success = await UserService.delete(id as string);
+
+        if (!success) {
+            return response.status(400).json({ error: 'error in deleted user' });
+        }
+
+        response.status(200).json({ success: 'deleted user' });
+    }
 }
 
 export default new UserController();
--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -4,6 +4,11 @@ import UserController from '../controller/user.controller.ts';
 
 const router = Router();
 
+// Rotas de usuários
 router.get('/', UserController.findAll);
+router.get('/:id', UserController.findById);
+router.post('/', UserController.create);
+router.put('/:id', UserController.update);
+router.delete('/:id', UserController.delete);
 
 export default router;
